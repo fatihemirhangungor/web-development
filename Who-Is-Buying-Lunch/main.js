23f83d08edcd93ce0500589d7e1eca33c45db185
fatihemirhangungor <fatihangungor@gmail.com>
2021-12-01 00:22:59 +0300

    whos buying luch

---
+++ Who-Is-Buying-Lunch/main.js
@@ -0,0 +1,16 @@
+function whosPaying(names) {
+    
+    /******Don't change the code above*******/
+        
+        //Write your code here.
+        var rnd = Math.floor(Math.random() * names.length);
+        return names[rnd] + " is going to buy lunch today!";
+        
+        
+        
+    
+    
+    /******Don't change the code below*******/    
+    }
+
+    console.log(whosPaying(["fatih", "irem", "hayriye"]));
